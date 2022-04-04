68dff4dcdd99293450a1acebb5c57d6637c51747
Lucas <lucas.edgerly@gmail.com>
2022-04-04 18:46:46 -0400

    adicionado funções básicas

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,7 @@
 class CalcController {
 
     constructor(){
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -20,13 +21,115 @@ class CalcController {
     
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        })
+    }
+
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop(value);
+
+    }
+
+    soma(){
+
+    }
+
+    subtracao(){
+
+    }
+
+    multiplicacao(){
+
+    }
+    
+    divisao(){
+
+    }
+
+    porcento(){
+
+    }
+
+    igual(){
+
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError(){
+        this.displayCalc = "Erro";
+    }
+
+    execBtn(value){
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                this.soma();
+                break;
+            case 'subtracao':
+                this.subtracao();
+                break;
+            case 'multiplicacao':
+                this.multiplicacao();
+                break;
+            case 'divisao':
+                this.divisao();
+                break;
+            case 'porcento':
+                this.porcento();
+                break;
+            case 'igual':
+                this.igual();
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+        };
+    }
+
     initButtonsEvents(){
     
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
-        buttons.forEach((btn, index)=>{
-            btn.addEventListener('click', e=>{
+        buttons.forEach((btn, index) =>{
+            this.addEventListenerAll(btn, "click drag", e => {
+
+                let textBtn = btn.className.baseVal.replace("btn-","");
+                this.execBtn();
                 console.log(btn.className.baseVal.replace("btn-",""));
+
+            });
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+                btn.style.cursor = 'pointer';
             });
         });
 
